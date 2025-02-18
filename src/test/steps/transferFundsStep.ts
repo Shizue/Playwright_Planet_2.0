31494630b44b4bd9bcc8ea46d5017c479448ab4a
Flavia <flaviashizuekohiyama@gmail.com>
2025-02-18 12:41:20 +0000

    ident

--- src/test/steps/transferFundsStep.ts
+++ src/test/steps/transferFundsStep.ts
@@ -2,11 +2,10 @@
 import { Given, When, Then, setDefaultTimeout } from '@cucumber/cucumber';
 import { expect } from '@playwright/test';
 import { TransferFundPage } from '../pages/TransferFundPage';
-import assert = require('assert');
 
 setDefaultTimeout(60000 * 2);
 const { pageFixture } = require("../../hooks/pageFixture");
-let transferFundPage= require("../pages/TransferFundPage");
+let transferFundPage = require("../pages/TransferFundPage");
 
 Given('User navegate to the application', async function () {
     transferFundPage = new TransferFundPage(pageFixture.page);
@@ -19,18 +18,18 @@ Then('I should see the form', async function () {
 
 Then('I should see the fields', async function () {
 
-    expect( await transferFundPage.isHtmlVisible, 'Html exist');
-    expect( await transferFundPage.isBodyVisible, 'body exist');
-    expect( await transferFundPage.isHeaderVisible, 'Header exist');
-    expect( await transferFundPage.isLblFromAccountVisible, 'label From Account exist');
-    expect( await transferFundPage.isHtmlVisible, 'label From Account exist');
-    expect( await transferFundPage.isHtmlVisible, 'label To Account exist');
-    expect( await transferFundPage.isHtmlVisible, 'label To Account exist');
-    expect( await transferFundPage.isHtmlVisible, 'label Amount exist');
-    expect( await transferFundPage.isHtmlVisible, 'label Amount exist');
-    expect( await transferFundPage.isHtmlVisible, 'label Currency exist');
-    expect( await transferFundPage.isHtmlVisible, 'label Currency exist');
-    expect( await transferFundPage.isHtmlVisible, 'label Transfer Funds exist');
+    expect(await transferFundPage.isHtmlVisible, 'Html exist');
+    expect(await transferFundPage.isBodyVisible, 'body exist');
+    expect(await transferFundPage.isHeaderVisible, 'Header exist');
+    expect(await transferFundPage.isLblFromAccountVisible, 'label From Account exist');
+    expect(await transferFundPage.isHtmlVisible, 'label From Account exist');
+    expect(await transferFundPage.isHtmlVisible, 'label To Account exist');
+    expect(await transferFundPage.isHtmlVisible, 'label To Account exist');
+    expect(await transferFundPage.isHtmlVisible, 'label Amount exist');
+    expect(await transferFundPage.isHtmlVisible, 'label Amount exist');
+    expect(await transferFundPage.isHtmlVisible, 'label Currency exist');
+    expect(await transferFundPage.isHtmlVisible, 'label Currency exist');
+    expect(await transferFundPage.isHtmlVisible, 'label Transfer Funds exist');
 });
 
 When('I fill in the form with valid data', async function () {
@@ -38,12 +37,12 @@ When('I fill in the form with valid data', async function () {
 });
 
 When('I submit the form', async function () {
-        await transferFundPage.submitForm();
+    await transferFundPage.submitForm();
 });
 
 Then('I should see a success message', async function () {
     expect(transferFundPage.getMessageSuccess, "Message success exist");
-     expect(transferFundPage.getTransactionID, "Transaction ID exist");
+    expect(transferFundPage.getTransactionID, "Transaction ID exist");
 });
 
 When('I fill in the form with invalid data', async function () {
